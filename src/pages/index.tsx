fc2b27f72bdaa13242b349ca99a9208be9aaea59
Pasha <pashamalyshkin2003@gmail.com>
2023-11-10 17:13:14 +0200

    added keepPreviousData property in pokemons query / added btn disabling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,8 +8,18 @@ export default function Home() {
   const [ids, setIds] = useState(() => getOptionsForVote());
   const [firstPokemonID, secondPokemonID] = ids;
 
-  const firstPokemon = trpc.getPokemonById.useQuery({ id: firstPokemonID });
-  const secondPokemon = trpc.getPokemonById.useQuery({ id: secondPokemonID });
+  const firstPokemon = trpc.getPokemonById.useQuery(
+    { id: firstPokemonID },
+    {
+      keepPreviousData: true,
+    }
+  );
+  const secondPokemon = trpc.getPokemonById.useQuery(
+    { id: secondPokemonID },
+    {
+      keepPreviousData: true,
+    }
+  );
   const { mutate: voteForPokemon } = trpc.castVote.useMutation();
 
   const dataLoaded =
@@ -38,19 +48,26 @@ export default function Home() {
     <div className="h-screen w-screen flex flex-col justify-center items-center">
       <div className="text-2xl text-center">Which Pokemon is rounder?</div>
       <div className="p-2"></div>
-      {dataLoaded && (
-        <div className="border rounded p-8 flex justify-between max-w-2xl items-center">
-          <PokemonListing
-            pokemon={firstPokemon.data}
-            voteForRoundest={() => voteForRoundest(firstPokemonID)}
-          />
-          <div className="p-8">VS</div>
-          <PokemonListing
-            pokemon={secondPokemon.data}
-            voteForRoundest={() => voteForRoundest(secondPokemonID)}
-          />
-        </div>
-      )}
+      <div
+        className="border rounded p-8 flex justify-between max-w-2xl items-center w-[659px] h-[405px]"
+        suppressHydrationWarning
+      >
+        {dataLoaded && (
+          <>
+            <PokemonListing
+              disabled={firstPokemon.isFetching}
+              pokemon={firstPokemon.data}
+              voteForRoundest={() => voteForRoundest(firstPokemonID)}
+            />
+            <div className="p-8">VS</div>
+            <PokemonListing
+              disabled={secondPokemon.isFetching}
+              pokemon={secondPokemon.data}
+              voteForRoundest={() => voteForRoundest(secondPokemonID)}
+            />
+          </>
+        )}
+      </div>
       <Link href="/results">See Results</Link>
     </div>
   );
@@ -59,7 +76,8 @@ export default function Home() {
 const PokemonListing: React.FC<{
   pokemon: { name: string; spriteUrl: string };
   voteForRoundest: () => void;
-}> = ({ pokemon, voteForRoundest }) => {
+  disabled: boolean;
+}> = ({ pokemon, voteForRoundest, disabled }) => {
   return (
     <div className="flex flex-col" suppressHydrationWarning>
       <Image
@@ -71,8 +89,9 @@ const PokemonListing: React.FC<{
       />
       <div className="text-xl text-center capitalize mb-4">{pokemon.name}</div>
       <button
+        disabled={disabled}
         onClick={voteForRoundest}
-        className="rounded-2xl text-indigo-900 flex items-center justify-center w-24 m-auto h-10 bg-yellow-300 hover:bg-green-300 hover:animate-pulse duration-300"
+        className="rounded-2xl cursor-pointer text-indigo-900 flex items-center justify-center w-24 m-auto h-10 bg-yellow-300 hover:bg-green-300 hover:animate-pulse duration-300 disabled:bg-slate-700"
       >
         Roundest
       </button>
--- src/server/trpc.ts
+++ src/server/trpc.ts
@@ -1,4 +1,7 @@
-import { initTRPC } from "@trpc/server";
+import { inferRouterOutputs, initTRPC } from "@trpc/server";
+import { AppRouter } from "./routers/_app";
 const t = initTRPC.create();
 export const router = t.router;
 export const publicProcedure = t.procedure;
+
+export type RouterOutputs = inferRouterOutputs<AppRouter>
