3e6b2ea0f2c5ffab41324e734648061e802ab053
Pasha <pashamalyshkin2003@gmail.com>
2023-11-10 15:01:09 +0200

    added some custom

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import { getOptionsForVote } from "@/utils/getRandomPokemon";
 import { trpc } from "@/utils/trpc";
 import { useState } from "react";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   const [ids, setIds] = useState(() => getOptionsForVote());
@@ -10,9 +11,12 @@ export default function Home() {
   const firstPokemon = trpc.getPokemonById.useQuery({ id: firstPokemonID });
   const secondPokemon = trpc.getPokemonById.useQuery({ id: secondPokemonID });
   const { mutate: voteForPokemon } = trpc.castVote.useMutation();
-  if (firstPokemon.isLoading || secondPokemon.isLoading) {
-    return null;
-  }
+
+  const dataLoaded =
+    !firstPokemon.isLoading &&
+    firstPokemon.data &&
+    !secondPokemon.isLoading &&
+    secondPokemon.data;
 
   const voteForRoundest = (selected: number) => {
     if (selected === firstPokemonID) {
@@ -34,33 +38,26 @@ export default function Home() {
     <div className="h-screen w-screen flex flex-col justify-center items-center">
       <div className="text-2xl text-center">Which Pokemon is rounder?</div>
       <div className="p-2"></div>
-      <div className="border rounded p-8 flex justify-between max-w-2xl items-center">
-        {!secondPokemon.isLoading &&
-          !firstPokemon.isLoading &&
-          secondPokemon.data &&
-          firstPokemon.data && (
-            <PokemonListing
-              pokemon={firstPokemon.data}
-              voteForRoundest={() => voteForRoundest(firstPokemonID)}
-            />
-          )}
-        <div className="p-8">Vs</div>
-        {!secondPokemon.isLoading &&
-          !firstPokemon.isLoading &&
-          secondPokemon.data &&
-          firstPokemon.data && (
-            <PokemonListing
-              pokemon={secondPokemon.data}
-              voteForRoundest={() => voteForRoundest(secondPokemonID)}
-            />
-          )}
-      </div>
+      {dataLoaded && (
+        <div className="border rounded p-8 flex justify-between max-w-2xl items-center">
+          <PokemonListing
+            pokemon={firstPokemon.data}
+            voteForRoundest={() => voteForRoundest(firstPokemonID)}
+          />
+          <div className="p-8">VS</div>
+          <PokemonListing
+            pokemon={secondPokemon.data}
+            voteForRoundest={() => voteForRoundest(secondPokemonID)}
+          />
+        </div>
+      )}
+      <Link href="/results">See Results</Link>
     </div>
   );
 }
 
 const PokemonListing: React.FC<{
-  pokemon: { name: string, spriteUrl: string };
+  pokemon: { name: string; spriteUrl: string };
   voteForRoundest: () => void;
 }> = ({ pokemon, voteForRoundest }) => {
   return (
@@ -72,12 +69,10 @@ const PokemonListing: React.FC<{
         alt="first pokemon"
         layout="fixed"
       />
-      <div className="text-xl text-center capitalize mt-[-2rem]">
-        {pokemon.name}
-      </div>
+      <div className="text-xl text-center capitalize mb-4">{pokemon.name}</div>
       <button
         onClick={voteForRoundest}
-        className="border-2 border-neutral-700 flex items-center justify-center w-24 m-auto "
+        className="rounded-2xl text-indigo-900 flex items-center justify-center w-24 m-auto h-10 bg-yellow-300 hover:bg-green-300 hover:animate-pulse duration-300"
       >
         Roundest
       </button>
--- src/pages/results.tsx
+++ src/pages/results.tsx
@@ -2,12 +2,10 @@ import type { GetServerSideProps } from "next";
 import { prisma } from "@/server/utils/prisma";
 import { inferAsyncReturnType } from "@trpc/server";
 import Image from "next/image";
+import Link from "next/link";
 
 const getPokemonInOrder = async () => {
   return await prisma.pokemon.findMany({
-    orderBy: {
-      VoteFor: { _count: "desc" },
-    },
     select: {
       id: true,
       name: true,
@@ -22,39 +20,81 @@ const getPokemonInOrder = async () => {
   });
 };
 type PokemonQueryResult = inferAsyncReturnType<typeof getPokemonInOrder>;
-
+const generateCountPercent = (pokemon: PokemonQueryResult[number]) => {
+  const { VoteFor, VoteAgainst } = pokemon._count;
+  if (VoteFor + VoteAgainst === 0) {
+    return 0;
+  }
+  return Math.round((VoteFor / (VoteFor + VoteAgainst)) * 100);
+};
 const PokemonListing: React.FC<{
   pokemon: PokemonQueryResult[number];
-}> = (props) => {
+}> = ({ pokemon }) => {
+
+  const containerStyles = "flex border-b p-2 items-center justify-between"
+
+  const getBgColor = (percent: number) => {
+    switch (true) {
+      case percent > 75:
+        return ' bg-green-400';
+      case percent >= 50:
+        return ' bg-yellow-400';
+      case percent > 0: 
+        return ' bg-red-400';
+      default:
+        return ' bg-slate-400';
+    }
+  }
+
   return (
-    <div className="flex border-b p-4 items-center w-full">
-      <Image
-        src={props.pokemon.spriteUrl}
-        width="64"
-        height="64"
-        alt="first pokemon"
-        layout="fixed"
-      />
-      <div className="capitalize">{props.pokemon.name}</div>
+    <div className={containerStyles + getBgColor(generateCountPercent(pokemon))} >
+      <div className="flex items-center">
+        <Image
+          src={pokemon.spriteUrl}
+          width="64"
+          height="64"
+          alt="first pokemon"
+          layout="fixed"
+        />
+        <div className="capitalize">{pokemon.name}</div>
+      </div>
+      <div className="pr-8">{generateCountPercent(pokemon)+ "%"}</div>
     </div>
   );
 };
 
 const ResultsPage: React.FC<{
   pokemon: PokemonQueryResult;
-}> = (props) => {
-  if (!props.pokemon) {
+}> = ({ pokemon }) => {
+  if (!pokemon) {
     return <div>loading</div>;
   }
+  const sortedPokemons = [...pokemon].sort((a, b) => (
+    generateCountPercent(b) - generateCountPercent(a)
+  ))
   return (
-    <div className="flex flex-col items-center">
-      <h2 className="text-2xl p-4">Results</h2>
-      <div className="flex flex-col items-center border w-full max-w-2xl">
-        {props.pokemon.map((currentPokemon) => {
-          return (
-            <PokemonListing key={currentPokemon.id} pokemon={currentPokemon} />
-          );
-        })}
+    <div className="relative">
+      <Link
+        href="/"
+        className="rounded-2xl flex items-center justify-center w-24 m-auto h-10 bg-blue-400 hover:bg-blue-600 duration-300 absolute left-6 top-6"
+      >
+        Back
+      </Link>
+      <div className="flex flex-col items-center">
+        <div className="flex p-4 gap-4">
+          <h2 className="text-2xl items-center">Results</h2>
+        </div>
+
+        <div className="flex flex-col border w-full max-w-2xl">
+          {sortedPokemons.map((currentPokemon) => {
+            return (
+              <PokemonListing
+                key={currentPokemon.id}
+                pokemon={currentPokemon}
+              />
+            );
+          })}
+        </div>
       </div>
     </div>
   );
